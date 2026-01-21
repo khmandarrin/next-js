dce4d9d08b23a5c28bb0fb8d961195147e56bcf9
khmandarrin <jeong-ga-eun@jeong-ga-eun-ui-MacBookAir.local>
2026-01-21 11:53:20 +0900

    디비 불러오는 로직 수정

---
+++ env.ts
@@ -0,0 +1,24 @@
+# Recommended for most uses
+DATABASE_URL=postgresql://neondb_owner:npg_9e2GXbPhfWSp@ep-round-water-ah8ff9pr-pooler.c-3.us-east-1.aws.neon.tech/neondb?sslmode=require
+
+# For uses requiring a connection without pgbouncer
+DATABASE_URL_UNPOOLED=postgresql://neondb_owner:npg_9e2GXbPhfWSp@ep-round-water-ah8ff9pr.c-3.us-east-1.aws.neon.tech/neondb?sslmode=require
+
+# Parameters for constructing your own connection string
+PGHOST=ep-round-water-ah8ff9pr-pooler.c-3.us-east-1.aws.neon.tech
+PGHOST_UNPOOLED=ep-round-water-ah8ff9pr.c-3.us-east-1.aws.neon.tech
+PGUSER=neondb_owner
+PGDATABASE=neondb
+PGPASSWORD=npg_9e2GXbPhfWSp
+
+# Parameters for Vercel Postgres Templates
+POSTGRES_URL=postgresql://neondb_owner:npg_9e2GXbPhfWSp@ep-round-water-ah8ff9pr-pooler.c-3.us-east-1.aws.neon.tech/neondb?sslmode=require
+POSTGRES_URL_NON_POOLING=postgresql://neondb_owner:npg_9e2GXbPhfWSp@ep-round-water-ah8ff9pr.c-3.us-east-1.aws.neon.tech/neondb?sslmode=require
+POSTGRES_USER=neondb_owner
+POSTGRES_HOST=ep-round-water-ah8ff9pr-pooler.c-3.us-east-1.aws.neon.tech
+POSTGRES_PASSWORD=npg_9e2GXbPhfWSp
+POSTGRES_DATABASE=neondb
+POSTGRES_URL_NO_SSL=postgresql://neondb_owner:npg_9e2GXbPhfWSp@ep-round-water-ah8ff9pr-pooler.c-3.us-east-1.aws.neon.tech/neondb
+POSTGRES_PRISMA_URL=postgresql://neondb_owner:npg_9e2GXbPhfWSp@ep-round-water-ah8ff9pr-pooler.c-3.us-east-1.aws.neon.tech/neondb?connect_timeout=15&sslmode=require
+
+AUTH_SECRET=f8in/J7apQSnYaFJHuXbuYnFIUMbIpTW/UW0WyHU/G0=
